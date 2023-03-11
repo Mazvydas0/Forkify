ddb7ac254bd6ac88303484a03629b485c1c1f8ec
Mazvydas0 <mazvydas.svirskas@gmail.com>
2023-03-11 15:35:33 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'c72e0694-4622-4f66-9678-21255e2a33b6';
+export const MODAL_CLOSE_SEC = 2.5;
---
+++ src/js/views/addRecipeView.js
@@ -0,0 +1,46 @@
+import View from './View.js';
+import icons from 'url:../../img/icons.svg'; // Parcel 2
+
+class AddRecipeView extends View {
+  _parentElement = document.querySelector('.upload');
+  _message = 'Recipe was successfully uploaded :)';
+
+  _window = document.querySelector('.add-recipe-window');
+  _overlay = document.querySelector('.overlay');
+  _btnOpen = document.querySelector('.nav__btn--add-recipe');
+  _btnClose = document.querySelector('.btn--close-modal');
+
+  constructor() {
+    super();
+    this._addHandlerShowWindow();
+    this._addHandlerHideWindow();
+  }
+
+  toggleWindow() {
+    this._overlay.classList.toggle('hidden');
+    this._window.classList.toggle('hidden');
+  }
+
+  _addHandlerShowWindow() {
+    this._btnOpen.addEventListener('click', this.toggleWindow.bind(this)); // to bind to the main class object, and not the element, calling the event, as it would be without bind
+  }
+
+  _addHandlerHideWindow() {
+    this._btnClose.addEventListener('click', this.toggleWindow.bind(this));
+    this._overlay.addEventListener('click', this.toggleWindow.bind(this));
+  }
+
+  addHandlerUpload(handler) {
+    this._parentElement.addEventListener('submit', function (e) {
+      e.preventDefault();
+      const dataArr = [...new FormData(this)]; // because we're inside handler function and this points to form (parentElement)
+      const data = Object.fromEntries(dataArr);
+      location.reload()
+      handler(data);
+    });
+  }
+
+  _generateMarkup() {}
+}
+
+export default new AddRecipeView();
